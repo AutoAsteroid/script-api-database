aa8b0fff17de99578d30c1ef9b651a8dfda28b43
Jordan Nguyen <j.nguyen.app@gmail.com>
2026-07-26 03:29:53 -0700

    Added external-db.js file

---
+++ external-db.js
@@ -0,0 +1,7 @@
+import { http, HttpRequest, HttpHeader, HttpRequestMethod, HttpResponse } from "@minecraft/server-net";
+
+/**
+ * ONLY import this file if you are on a BDS server, and intend on using external JSON file databases.
+ */
+
+console.warn("Meow meow meow...");
