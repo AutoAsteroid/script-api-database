e03355a4b3610eefcfe290fa1bf7877123ce1e40
Jordan Nguyen <j.nguyen.app@gmail.com>
2026-07-27 18:08:54 -0700

    Updated external-db documentation

--- external-db.js
+++ external-db.js
@@ -2,20 +2,22 @@ import { system } from "@minecraft/server";
 import { http, HttpRequest, HttpRequestMethod, HttpHeader } from "@minecraft/server-net";
 
 /**
- * ONLY import this file if you are on a BDS server, and intend on using external JSON file databases.
+ * Only import this file if you want to be able to store cold storage 
+ * locally in JSON files and are able to run the external go program
  */
 
 const HTTP_ADDRESS = "http://127.0.0.1:8080";
-const AUTH_TOKEN = "script-api";
+const AUTH_TOKEN = "script-api"; // Protect from local programs
 
 /**
- * Reads a JSON file stored on the external backend
- * @param {string} file The file identifier (excluding .json)
+ * Reads from a JSON file stored on the external local json/ folder
+ * @param {string} file The file identifier path (excluding .json)
  * @returns {Promise<any|null>} The parsed object or null on failure
  */
 async function loadJSON(file) {
     const request = new HttpRequest(HTTP_ADDRESS + "/loadJSON");
 
+    // Target file is passed in the header rather than in the URL
     request.method = HttpRequestMethod.Get;
     request.headers = [
         new HttpHeader("Content-Type", "application/json"),
@@ -23,6 +25,7 @@ async function loadJSON(file) {
         new HttpHeader("file", file)
     ];
 
+    // Response will be return null if the file does not exist
     try {
         const response = await http.request(request);
         return JSON.parse(response);
@@ -32,16 +35,19 @@ async function loadJSON(file) {
 }
 
 /**
- * Writes a JSON file stored on the external backend
- * @param {string} file The file identifier (excluding .json)
- * @param {any} data The data object to store
- * @returns {Promise<boolean>} Success status
+ * Writes to a JSON file stored on the external local json/ folder
+ * @param {string} file The file identifier path (excluding .json)
+ * @param {any} data The data object to store in the json file
+ * @returns {Promise<boolean>} Success status if the write worked
  */
 async function saveJSON(file, data) {
     const request = new HttpRequest(HTTP_ADDRESS + "/saveJSON");
 
+    // Target file is passed in the header rather than body
     request.method = HttpRequestMethod.Post;
-    request.body = JSON.stringify(data);
+
+    // Go does not need to do any parsing on the actual JSON data
+    request.body = JSON.stringify(data); 
     request.headers = [
         new HttpHeader("Content-Type", "application/json"),
         new HttpHeader("auth", AUTH_TOKEN),
