4cb34ac63ddd29c01138a5cf5ded64730c5d43a4
Jordan Nguyen <j.nguyen.app@gmail.com>
2026-07-28 15:37:49 -0700

    Setup scoreboard class manager for scoreboard data

---
+++ scoreboard.js
@@ -0,0 +1,27 @@
+import { world } from "@minecraft/server";
+
+export class EntityScoreboard {
+    constructor() {
+
+    }
+
+    get() {
+
+    }
+
+    set() {
+
+    }
+
+    add() {
+
+    }
+
+    remove() {
+
+    }
+
+    reset() {
+
+    }
+}
