3aba8ac8ead00f6bf4b4ae9d8eb8c759ef80e740
Jordan <asteroidkitpvp@gmail.com>
2026-08-09 12:36:39 -0700

    Omit formatting in saveJSON for storage purposes

--- external-db.js
+++ external-db.js
@@ -48,7 +48,7 @@ async function saveJSON(file, data) {
     request.method = HttpRequestMethod.Post;
 
     // Go does not need to do any parsing on the actual JSON data
-    request.body = JSON.stringify(data, null, 4); 
+    request.body = JSON.stringify(data); 
     request.headers = [
         new HttpHeader("Content-Type", "application/json"),
         new HttpHeader("auth", AUTH_TOKEN),
